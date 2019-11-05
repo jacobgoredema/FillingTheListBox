d9ea9310241d713ec0dbf02edeb156f3bf26169d
jacob goredema <jacobgoredema@gmail.com>
2019-11-05 11:54:16 +0200

    initial Commit

---
+++ About.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="About.aspx.cs" Inherits="About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.</p>
+<p>&nbsp;</p>
+<p>
+    <asp:Label ID="Label1" runat="server" Text="Select Author:"></asp:Label>
+    <asp:DropDownList ID="lstAuthor" runat="server" Height="35px" Width="221px">
+    </asp:DropDownList>
+</p>
+<p>&nbsp;</p>
+<p>
+    <asp:Label ID="lblResult" runat="server"></asp:Label>
+</p>
+</asp:Content>
---
+++ About.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class About : Page
+{
+    private string connectionString = WebConfigurationManager.ConnectionStrings["Pubs"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            FillAuthorList();
+        }
+    }
+
+    private void FillAuthorList()
+    {
+        lstAuthor.Items.Clear();
+        // Define the select statement
+        string selectSQL = "SELECT au_lname,au_fname,au_id FROM Authors";
+
+        // define the ADO.NET objects
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlCommand cmd = new SqlCommand(selectSQL,conn);
+
+        SqlDataReader reader;
+
+        // try to open database and read information
+        try
+        {
+            conn.Open();
+            reader = cmd.ExecuteReader();
+
+            //loop
+            while (reader.Read())
+            {
+                ListItem newItem = new ListItem();
+                newItem.Text = reader["au_lname"] + "," + reader["au_fname"];
+                newItem.Value = reader["au_id"].ToString();
+                lstAuthor.Items.Add(newItem);
+            }
+
+            reader.Close();
+        }
+        catch(Exception err)
+        {
+            lblResult.Text = "Error reading list of names.";
+            lblResult.Text += err.Message;
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
